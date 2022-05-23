2afa7b95ff3eb1ef9379bfca1eacbba40fb174c5
Sivar-Developer <fragmatica.dc@gmail.com>
2022-05-23 12:20:51 +0300

    Account settings

--- core/views.py
+++ core/views.py
@@ -1,12 +1,31 @@
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 
 from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            
+        user_profile.profileimg = image
+        user_profile.bio = request.POST['bio']
+        user_profile.location = request.POST['location']
+        user_profile.save()
+        return redirect('settings')
+    return render(request, 'setting.html', { 'user_profile': user_profile })
 
 def signup(request):
     if request.method == 'POST':
@@ -27,13 +46,15 @@ def signup(request):
                 user.save()
                 
                 # login user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 
                 # create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
                 
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -57,6 +78,7 @@ def signin(request):
     else:
         return render(request, 'signin.html')
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
